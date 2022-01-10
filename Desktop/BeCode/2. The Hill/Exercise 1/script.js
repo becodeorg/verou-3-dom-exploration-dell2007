67ed78bd8a4f156f70bc223c834fbff3363d938b
Dericioso <dery.lantigua@gmail.com>
2022-01-10 11:26:35 +0100

    Ramdon colors added

--- Desktop/BeCode/2. The Hill/Exercise 1/script.js
+++ Desktop/BeCode/2. The Hill/Exercise 1/script.js
@@ -1,3 +1,12 @@
 document.title = "Modifying the DOM";
 console.log(document.title)
 
+document.body.style.backgroundColor = "#FF69Br";
+
+function generateRandomColorRgb() {
+    var red = Math.round(Math.random() * 255);
+    var green = Math.round(Math.random() * 255);
+    var blue = Math.round(Math.random() * 255);
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
+document.body.style.backgroundColor = generateRandomColorRgb();
