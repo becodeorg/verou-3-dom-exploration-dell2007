157b05f701a6f64f04dae60c2a9e817b290c654b
Dericioso <dery.lantigua@gmail.com>
2022-01-10 14:27:24 +0100

    Img tags loop

--- Desktop/BeCode/2. The Hill/Exercise 1/Selector/script.js
+++ Desktop/BeCode/2. The Hill/Exercise 1/Selector/script.js
@@ -1,6 +1,10 @@
-
 var newTitle= document.getElementsByClassName("important");
 var i;
 for ( i = 0; i < newTitle.length; i++) {
     newTitle[i].setAttribute("title", "This is an important item");
+}
+
+var tags = document.getElementsByTagName("img");
+for (var i=0, max=tags.length; i < max; i++);{
+console.log(tags, "i");
 }
