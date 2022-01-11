f7c0d0fdf27b90a48d63f087c1bdfb998ec9ba08
Dericioso <dery.lantigua@gmail.com>
2022-01-11 15:25:36 +0100

    Second part added

--- Desktop/BeCode/2. The Hill/Exercise 1/3. Create/script.js
+++ Desktop/BeCode/2. The Hill/Exercise 1/3. Create/script.js
@@ -1,5 +1,6 @@
 const promo = ["Ahmad", "Jian", "Michael", "Dante", "Muhammet", "Nicolas", "Jordy", "Beryl", "Cynthia", "Ruben", "Magali", "Jawid",
-    "Koen", "Kevin", "Benjamin", "Dery", "Michelle", "Colin", "Feruz", "Pieter", "Ellen", "Sara"];
+    "Koen", "Kevin", "Benjamin", "Dery", "Michelle", "Colin", "Feruz", "Pieter", "Ellen", "Sara"
+];
 
 // Add a random color 
 function generateRandomColorRgb() {
@@ -9,19 +10,30 @@ function generateRandomColorRgb() {
     return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
+// Change text color depending on the background
+function getTextColor(rgba) {
+    rgba = rgba.match(/\d+/g);
+    if ((rgba[0] * 0.299) + (rgba[1] * 0.587) + (rgba[2] * 0.114) > 127.5) {
+        return 'black';
+    } else {
+        return 'white';
+    }
+}
+
 //Add a new element
 function addElement(name) {
     const newSection = document.createElement("section"); //Create a section
     newSection.style.backgroundColor = generateRandomColorRgb();
+    newSection.style.color = getTextColor (newSection.style.backgroundColor);
     const article = document.querySelector("article"); //Select the article
-    article.appendChild(newSection); //Append the new section
-    const newPar = document.createElement("p");
-    newSection.appendChild(newPar);
-    const newContent = document.createTextNode(name);
-    newPar.appendChild(newContent);
+    article.appendChild(newSection); //Append the section to the article
+    const newPar = document.createElement("p"); //Create a P
+    newSection.appendChild(newPar); //Append the P to the section
+    const newContent = document.createTextNode(name); //Add text from the array which was organize in the loop
+    newPar.appendChild(newContent); //Append the text to the P
 }
 
-
+//Loop added to select all the names individually
 for (let i = 0; i < promo.length; i++) {
     const name = promo[i];
     addElement(name);
