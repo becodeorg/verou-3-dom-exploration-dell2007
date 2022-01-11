27b94f88a643b1fbe9e5e9a4c0c390be111736e1
Dericioso <dery.lantigua@gmail.com>
2022-01-11 14:35:44 +0100

    First part of the exercise done

--- Desktop/BeCode/2. The Hill/Exercise 1/3. Create/script.js
+++ Desktop/BeCode/2. The Hill/Exercise 1/3. Create/script.js
@@ -1,12 +1,30 @@
-var promo = ("Ahmad", "Jian", "Michael", "Dante", "Muhammet", "Nicolas", "Jordy", "Beryl", "Cynthia", "Ruben", "Magali", "Jawid", 
-"Koen", "Kevin", "Benjamin", "Dery", "Michelle", "Colin", "Feruz", "Pieter", "Ellen", "Sara");
+const promo = ["Ahmad", "Jian", "Michael", "Dante", "Muhammet", "Nicolas", "Jordy", "Beryl", "Cynthia", "Ruben", "Magali", "Jawid",
+    "Koen", "Kevin", "Benjamin", "Dery", "Michelle", "Colin", "Feruz", "Pieter", "Ellen", "Sara"];
 
-document.body.onload = addElement;
+// Add a random color 
+function generateRandomColorRgb() {
+    var red = Math.round(Math.random() * 255);
+    var green = Math.round(Math.random() * 255);
+    var blue = Math.round(Math.random() * 255);
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
 
-const newP = document.createElement("section");
+//Add a new element
+function addElement(name) {
+    const newSection = document.createElement("section"); //Create a section
+    newSection.style.backgroundColor = generateRandomColorRgb();
+    const article = document.querySelector("article"); //Select the article
+    article.appendChild(newSection); //Append the new section
+    const newPar = document.createElement("p");
+    newSection.appendChild(newPar);
+    const newContent = document.createTextNode(name);
+    newPar.appendChild(newContent);
+}
 
-const newContent = document.createTextNode(promo);
 
-newP.appendChild(newContent);
+for (let i = 0; i < promo.length; i++) {
+    const name = promo[i];
+    addElement(name);
+}
 
-const currentP = document
+// document.body.onload = addElement;
