84700309b3881773ed67494b575cfe0713502f75
Dericioso <dery.lantigua@gmail.com>
2022-01-17 15:51:07 +0100

    Create new div and log action

--- Desktop/BeCode/2. The Hill/Exercise 1/5. Event/script.js
+++ Desktop/BeCode/2. The Hill/Exercise 1/5. Event/script.js
@@ -5,8 +5,17 @@ function getElapsedTime() {
 }
 
 function clickOnSquare(e) {
+    const existDiv = document.querySelector('.displayedsquare-wrapper');
     console.log(e.target.classList[1])
     console.log(getElapsedTime())
+    const newDiv = document.createElement('div');
+    newDiv.className = 'displayedsquare' + ' ' + e.target.classList[1];
+    existDiv.appendChild(newDiv);
+
+    const newList = document.createElement('li');
+    const existList = document.querySelector('ul');
+    existList.appendChild(newList);
+    newList.innerText = '[' + getElapsedTime() + ']' + ' Created a new ' + e.target.classList[1] + ' square';
 }
 
 const actionsquares = document.querySelectorAll('.actionsquare')
