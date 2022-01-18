3c85c4fc4c13eb7d09d0b868a27fb3f53f013fa0
Dericioso <dery.lantigua@gmail.com>
2022-01-18 15:43:45 +0100

    Message visible by age

--- Desktop/BeCode/2. The Hill/Exercise 1/6. Form/script.js
+++ Desktop/BeCode/2. The Hill/Exercise 1/6. Form/script.js
@@ -1,9 +1,25 @@
 const form = document.getElementById('firstname');
 
 const displayFN = (event) => {
-    
     event.preventDefault();
+
     const newFN = event.target.value;
     document.getElementById('display-firstname').innerHTML = newFN;
 }
 form.addEventListener('keyup', displayFN);
+
+const form2 = document.getElementById('age');
+const hardTrue = document.getElementById('a-hard-truth');
+
+const displayAge = (event) => {
+    event.preventDefault();
+
+    const newAge = event.target.value;
+    if (newAge < 18) {
+        displayAge.innerHTML = "none";
+    } else {
+        hardTrue.style.visibility = 'visible';
+        displayAge.innerHTML = hardTrue;
+    }
+}
+form2.addEventListener('keyup', displayAge);
