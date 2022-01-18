0c9a715d65902c2ba971635cfa8543cd81d3cc30
Dericioso <dery.lantigua@gmail.com>
2022-01-18 14:35:25 +0100

    Delete the extra boxes

--- Desktop/BeCode/2. The Hill/Exercise 1/5. Event/script.js
+++ Desktop/BeCode/2. The Hill/Exercise 1/5. Event/script.js
@@ -5,8 +5,8 @@ function getElapsedTime() {
 }
 
 function clickOnSquare(e) {
-    console.log(e.target.classList[1])
-    console.log(getElapsedTime())
+    console.log(e.target.classList[1]);
+    console.log(getElapsedTime());
 
     //Create a new div
     const existDiv = document.querySelector('.displayedsquare-wrapper');
@@ -16,37 +16,34 @@ function clickOnSquare(e) {
 
     //Duplicate the colors from the existing div boxes
     const boxColor = e.target.classList[1];
-    newDiv.classList.add(boxColor);
+    newDiv.classList.add(boxColor); //When adding a new class with .classList it will create a new one without deleting existing ones 
 
-    //Create a log with the action done
+    //Create a log with when the action is done
     const newList = document.createElement('li');
     const existList = document.querySelector('ul');
     existList.appendChild(newList);
     newList.innerText = '[' + getElapsedTime() + ']' + ' Created a new ' + boxColor + ' square';
+
+    //Delete the duplicated boxes pressing s
+        document.addEventListener('keydown', function (event) {
+        if (event.key === 's') {
+            existDiv.innerHTML = " ";
+        }
+    })
+
+    //Creates alert pops-up
+    // const alertBox = document.getElementsByClassName('.displayedsquare');
+    // document.addEventListener('click', event => {
+    //     existDiv.textContent = alert(boxColor);
+    // })
 }
 
-const actionsquares = document.querySelectorAll('.actionsquare')
+const actionsquares = document.querySelectorAll('.actionsquare');
 for (let actionsquare of actionsquares) {
     actionsquare.addEventListener('click', clickOnSquare);
 }
 
 // Changing background color
-const background = document.getElementsByTagName('body')
-const sameList = document.querySelector('ul');
-document.addEventListener('keydown', function (event) {
-    if (event.key === ' ') {
-        backColor()
-        //Create a log when background change color
-        const otherList = document.createElement('li');
-        sameList.appendChild(otherList);
-        otherList.innerText = '[' + getElapsedTime() + ']' + ' Changed background color ';
-    }
-    //Delete the log pressing i 
-    if (event.key === 'i') {
-        sameList.innerHTML = " ";
-    }
-});
-
 function backColor() {
     document.body.style.backgroundColor = randomColor();
 }
@@ -57,3 +54,19 @@ function randomColor() {
     var blue = Math.round(Math.random() * 255);
     return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
+
+const background = document.getElementsByTagName('body');
+const sameList = document.querySelector('ul');
+document.addEventListener('keydown', function (event) {
+    if (event.key === ' ') {
+        backColor();
+        //Create a log when background change color
+        const otherList = document.createElement('li');
+        sameList.appendChild(otherList);
+        otherList.innerText = '[' + getElapsedTime() + ']' + ' Changed background color ';
+    }
+    //Delete the log pressing i 
+    if (event.key === 'i') {
+        sameList.innerHTML = " ";
+    }
+})
