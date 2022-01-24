8b5be471acea252305a57a8e0b189c94c375dd1d
Dericioso <dery.lantigua@gmail.com>
2022-01-24 11:13:14 +0100

    Search bar working

--- Desktop/BeCode/2. The Hill/Exercise 1/Collection Project/script.js
+++ Desktop/BeCode/2. The Hill/Exercise 1/Collection Project/script.js
@@ -7,6 +7,7 @@ const existDiv = document.querySelector("div");
 existDiv.className = "container"; //Container to manage the whole body
 const gridCont = document.createElement("div");
 gridCont.className = "grid-container";
+gridCont.setAttribute("id", "search");
 existDiv.appendChild(gridCont);
 
 for (const game of games) {
@@ -14,7 +15,7 @@ for (const game of games) {
     newDiv.className = "card"; //Class for styling
 
     const nPic = document.createElement("img");
-    nPic.src = game.picture; 
+    nPic.src = game.picture;
     nPic.textContent = game.picture;
     nPic.className = "pic";
     newDiv.appendChild(nPic);
@@ -23,8 +24,9 @@ for (const game of games) {
     newDiv.appendChild(otherDiv);
     otherDiv.className = "text";
 
-    const nTitle = document.createElement("h2"); 
+    const nTitle = document.createElement("h2");
     nTitle.textContent = game.name;
+    nTitle.setAttribute("id", "name");
     otherDiv.appendChild(nTitle);
 
     const year = document.createElement("p");
@@ -56,4 +58,38 @@ for (const game of games) {
     trailer.appendChild(utube);
 
     gridCont.appendChild(newDiv);
+}
+
+//Hover effects on each card 
+const hoverEffect = document.querySelectorAll(".card");
+hoverEffect.forEach(function hoveredCard(cardSelected) {
+    cardSelected.onmouseover = function () {
+        cardSelected.style.transform = "scale(1.10, 1.10)";
+        cardSelected.style.zIndex = 100;
+        cardSelected.style.transition = "0.30s";
+        cardSelected.style.boxShadow = "0 0 0 9999px #000000b0";
+    };
+    cardSelected.onmouseout = function () {
+        cardSelected.style.transform = "scale(1, 1)";
+        cardSelected.style.zIndex = 0;
+        cardSelected.style.boxShadow = "0 0 0 9999px #00000000";
+    };
+})
+
+//Search bar
+const searchBar = document.getElementById("myInput");
+searchBar.onkeyup = function () {
+    let input = searchBar.value.toLowerCase();
+    let cardName = document.querySelectorAll(".card");
+
+    cardName.forEach((el) => {
+        let elementTitle = el.children[1].children[0].innerHTML;
+
+        if (elementTitle.toLowerCase().includes(input)) {
+            el.style.display = "block";
+            el.classList.add("card");
+        } else {
+            el.style.display = "none";
+        }
+    })
 }
